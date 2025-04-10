60f62e94ea38f68045331cad6d65c3a5760fbd57
Kalrex <zarconxorp@gmail.com>
2025-04-10 20:54:23 +0800

    update

---
+++ src/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  prefix: 'p!',
+  owner: ["715783278237450280", "937998965754986526"],
+  token: process.env.TOKEN,
+  ERR_WEB: process.env.webhk
+};
--- src/config/index.ts
+++
@@ -1,4 +0,0 @@
-export const BOT_CONFIG = {
-  developers: ['715783278237450280'],
-  guildId: '840157628628729857'
-};
